eec4e9270b1f7a796617c5e9e98f6e378a80e545
AlNekoo <alvaro.alcaraz.roma@gmail.com>
2022-01-30 12:51:17 +0100

    2.0 Se agregan comentarios Javadoc

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
 package cuentas;
+
+/**
+ * 
+ * @author Alvaro
+ *@version 1.0
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +14,12 @@ public class Main {
 
         operativa_cuenta();
     }
+    
+    /**
+     * Método operativa_cuenta
+     * @param saldoActual indica el saldo 
+     */
 
-	/**
-	 * 
-	 */
 	private static void operativa_cuenta() {
 		CCuenta cuenta1;
 		double saldoActual;
@@ -18,7 +27,11 @@ public class Main {
 		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
+         
+        /**
+          * @excepcion para fallos al retirar
+          */
+        
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
